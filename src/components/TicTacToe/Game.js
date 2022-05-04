9ced53a8322271de5a1167ec10230aaeb48c02e5
David Starr <david@elegantcode.com>
2022-05-04 09:48:46 -0700

    removed history

--- src/components/TicTacToe/Game.js
+++ src/components/TicTacToe/Game.js
@@ -7,19 +7,14 @@ export default class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            history: [{
-                squares: Array(9).fill(null)
-            }],
-            stepNumber: 0,
+            squares: Array(9).fill(null),
             xIsNext: true,
         };
     }
 
     handleClick(i) {
 
-        const history = this.state.history;
-        const current = history[history.length - 1];
-        const squares = current.squares.slice();
+        const squares = this.state.squares.slice();
 
         if (calculateWinner(squares) || squares[i]) {
             return;
@@ -28,19 +23,15 @@ export default class Game extends React.Component {
         squares[i] = this.state.xIsNext ? 'X' : 'O';
 
         this.setState({
-            history: history.concat([{
-                squares: squares,
-            }]),
-            stepNumber: history.length,
+            squares: squares,
             xIsNext: !this.state.xIsNext,
         });
     }
 
     render() {
 
-        const history = this.state.history;
-        const current = history[this.state.stepNumber];
-        const winner = calculateWinner(current.squares);
+        const squares = this.state.squares;
+        const winner = calculateWinner(squares);
 
         let status;
         if (winner) {
@@ -59,7 +50,7 @@ export default class Game extends React.Component {
                 <div className="game">
                     <div className="game-board">
                         <Board
-                            squares={current.squares}
+                            squares={squares}
                             onClick={(i) => this.handleClick(i)}
                         />
                     </div>
