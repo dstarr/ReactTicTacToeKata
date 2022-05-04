8bab02b10a78fa23a0329c239f607aa6f5cd304a
David Starr <david@elegantcode.com>
2022-05-04 09:32:34 -0700

    removing jumpto functionality and adding css

---
+++ src/components/About/About.js
@@ -0,0 +1,10 @@
+export default function About(props) {
+    return (
+        <div>
+        
+        <h1>About the game</h1>
+
+        <p>The game is part of Intro to React tutorial by <b>Reactjs.org</b>.</p>
+        </div>
+    );
+}
--- src/components/TicTacToe/Game.js
+++ src/components/TicTacToe/Game.js
@@ -15,13 +15,6 @@ export default class Game extends React.Component {
         };
     }
 
-    jumpTo(step) {
-        this.setState({
-            stepNumber: step,
-            xIsNext: (step % 2) === 0,
-        });
-    }
-
     handleClick(i) {
 
         const history = this.state.history;
@@ -49,17 +42,6 @@ export default class Game extends React.Component {
         const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
 
-        const moves = history.map((step, move) => {
-            const desc = move ?
-                'Go to move #' + move :
-                'Go to game start';
-            return (
-                <li key={move}>
-                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
-                </li>
-            );
-        });
-
         let status;
         if (winner) {
             status = 'Winner: ' + winner;
@@ -70,16 +52,16 @@ export default class Game extends React.Component {
         return (
             <>
                 <h1>Game.js</h1>
+                <div className="game-info">
+                    <div className='game-status'>{status}</div>
+                </div>
+
                 <div className="game">
-                     <div className="game-board">
+                    <div className="game-board">
                         <Board
                             squares={current.squares}
                             onClick={(i) => this.handleClick(i)}
                         />
-                    </div>
-                    <div className="game-info">
-                        <div>{status}</div>
-                        <ol>{moves}</ol>
                     </div>
                 </div>
             </>
